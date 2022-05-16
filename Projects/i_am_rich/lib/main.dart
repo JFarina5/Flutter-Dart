e41c2da7764d7f44c87eba5f7f380586f56f4f06
john.farina <john.farina0824@gmail.com>
2022-05-15 22:55:12 -0400

    removed old projects, added new projects

---
+++ Projects/i_am_rich/lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
